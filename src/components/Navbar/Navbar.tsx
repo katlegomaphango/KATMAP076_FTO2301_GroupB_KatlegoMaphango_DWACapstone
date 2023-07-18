e51d510235a804cd2ed5c996a4e4205c8b2c940f
Katlego Maphango <mphangokatlego08@gmail.com>
2023-07-18 15:19:47 +0200

    refractoring and nav styling

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,22 @@
 import './Navbar.css'
+import logo from '../../../public/assets/meta/logo.png'
 
 const Navbar = () => {
     return (
         <>
-        <div>Navbar</div>
+            <nav className='nav'>
+                <div className="nav-container">
+                    <div className="logo">
+                        <img className='logo-img' src={logo} alt="" />
+                        <h1 className="logo-text">Plug 'n Play</h1>
+                    </div>
+                    <div className="login">
+                        <button className="btn login-btn">
+                            Log in
+                        </button>
+                    </div>
+                </div>
+            </nav>
         </>
     )
 }
