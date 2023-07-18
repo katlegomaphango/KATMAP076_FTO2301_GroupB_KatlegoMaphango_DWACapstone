0603d428915f235c0e4395c568f7256c7ef83dfb
Katlego Maphango <mphangokatlego08@gmail.com>
2023-07-18 22:01:44 +0200

    error

---
+++ src/components/Error/Error.tsx
@@ -0,0 +1,13 @@
+import './Error.css'
+
+const Error = () => {
+    return (
+        <>
+            <div className="error">
+                <h1>Something went wrong please refresh...</h1>
+            </div>
+        </>
+    )
+}
+
+export default Error
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,12 +1,13 @@
 import { SHOW } from '../assets/constants'
 import Loader from '../components/Loader/Loader'
+import Error from '../components/Error/Error'
 import { useGetAllShowsQuery } from '../redux/services/netlify'
 
 const LandingPage = () => {
     const { data, isFetching, error } = useGetAllShowsQuery([])
 
     if (isFetching) return <Loader />
-    
+    if (error) return <Error />
     console.log(data)
     const allShows: SHOW[] = data
 
