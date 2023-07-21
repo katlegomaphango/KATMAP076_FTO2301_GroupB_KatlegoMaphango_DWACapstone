8826959bae9d4fae4a42b9ab47f77fb22a807c80
Katlego Maphango <mphangokatlego08@gmail.com>
2023-07-21 03:36:11 +0200

    player handlers

--- src/components/MusicPlayer/index.tsx
+++ src/components/MusicPlayer/index.tsx
@@ -3,7 +3,7 @@
 import { useSelector, useDispatch } from "react-redux";
 import Controls from "./Controls";
 import { useEffect, useState } from "react";
-import { playPause } from "../../redux/features/playerSlice";
+import { nextEpisode, playPause, prevEpisode } from "../../redux/features/playerSlice";
 import { Box, styled } from "@mui/material";
 
 const StyledBox = styled(Box)({
@@ -33,15 +33,30 @@ const MusicPlayer = () => {
     }, [currentIndex])
 
     const handlePrevEpisode = () => {
-
+        if (currentIndex === 0) {
+            dispatch(prevEpisode(currentSeasonEpisodes.length - 1))
+        } else if (shuffle) {
+            dispatch(prevEpisode(Math.floor(Math.random() * currentSeasonEpisodes.length)))
+        } else {
+            dispatch(prevEpisode(currentIndex - 1))
+        }
     }
 
     const handlePlayPause = () => {
-        
+        if (!isActive) return
+
+        if (isPlaying) dispatch(playPause(false))
+        if (!isPlaying) dispatch(playPause(true))
     }
 
     const handleNextEpisode = () => {
-        
+        dispatch(playPause(false)) 
+
+        if(!shuffle) {
+            dispatch(nextEpisode((currentIndex + 1) % currentSeasonEpisodes.length))
+        } else {
+            dispatch(nextEpisode(Math.floor(Math.random() * currentSeasonEpisodes.length)))
+        }
     }
 
     return (
