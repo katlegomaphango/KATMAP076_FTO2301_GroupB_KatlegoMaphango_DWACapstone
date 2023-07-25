be352bf9dec4668a6ac21569df13e599e5dfe1b8
Katlego Maphango <mphangokatlego08@gmail.com>
2023-07-25 21:38:53 +0200

    home code

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,29 @@
+import { Box, styled } from "@mui/material"
+import HomeContent from "./LandingPage"
+import Favorites from "../components/Favorites/Favorites"
+
+type PROPS = {
+    token: {}
+}
+
+const MainBox = styled(Box)({
+    display: 'grid',
+    gridTemplateColumns: '68% 30%',
+    gap: '2%',
+    margin: 20,
+})
+
+const HomeLayout = (props: PROPS) => {
+    const {token} = props
+
+    return (
+        <>
+            <MainBox>
+                <HomeContent token={token} />
+                <Favorites token={token} />
+            </MainBox>
+        </>
+    )
+}
+
+export default HomeLayout
