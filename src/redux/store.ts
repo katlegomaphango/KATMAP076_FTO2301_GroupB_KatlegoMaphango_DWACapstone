9fdd9cb89f9ed45ea9d3a861f5faa8e90a0bfd04
Katlego Maphango <mphangokatlego08@gmail.com>
2023-07-27 22:59:07 +0200

    token slice

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import { netlifyApi } from "./services/netlify";
 import playerSlice from "./features/playerSlice";
 import favoriteSlice from "./features/favoriteSlice";
+import tokenSlice from "./features/tokenSlice";
 
 export const store = configureStore({
     reducer: {
         [netlifyApi.reducerPath]: netlifyApi.reducer,
         player: playerSlice,
-        favorite: favoriteSlice
+        favorite: favoriteSlice,
+        token: tokenSlice,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(netlifyApi.middleware)
 })
