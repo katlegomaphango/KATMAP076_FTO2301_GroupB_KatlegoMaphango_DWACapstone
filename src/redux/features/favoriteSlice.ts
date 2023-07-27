c70d1834753856a5e8722f3c88689db77dccf755
Katlego Maphango <mphangokatlego08@gmail.com>
2023-07-27 22:58:43 +0200

    add removed reducers

--- src/redux/features/favoriteSlice.ts
+++ src/redux/features/favoriteSlice.ts
@@ -1,17 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { EPISODE, User } from "../../assets/constants";
+import { EPISODE } from "../../assets/constants";
 
-type FAV = {
-    created_at?: string
-    description: string
-    episode_id: number
-    file: string
-    id?: string
-    season_id: number
-    show_id: number
-    title: string
-    user_id: string
-}
+// type FAV = {
+//     created_at?: string
+//     description: string
+//     episode_id: number
+//     file: string
+//     id?: string
+//     season_id: number
+//     show_id: number
+//     title: string
+//     user_id: string
+// }
 
 // const initialState: FAV = {
 //     created_at: '',
@@ -26,39 +26,60 @@ type FAV = {
 //     list_favorites: []
 // }
 
+type likedEP = {
+    episode: EPISODE
+    showTitle: string
+}
+
 type initialState = {
-    likedEpisodes: FAV[] | any[]
+    likedEpisodes: likedEP[] | any[]
+    isliked: boolean
+    likedEp: EPISODE
 }
 
 const initialState: initialState = {
-    likedEpisodes: []
+    likedEpisodes: [],
+    isliked: false,
+    likedEp: {
+        description: '',
+        episode: 0,
+        file: '',
+        title: ''
+    }
 }
 
 const favoriteSlice = createSlice({
     name: 'favorite',
     initialState,
     reducers: {
-        AddToLikedEpisodes: (state, action: { type: string, payload: FAV}) => {
+        AddToLikedEpisodes: (state, action: { type: string, payload: { episode: EPISODE, showTitle: string }}) => {
             state.likedEpisodes.push(action.payload)
+
+            state.likedEp = action.payload.episode
+            state.isliked = true
         },
         SetAllLikedEpisodes: (state, action: {type: string, payload: any[]}) => {
             state.likedEpisodes = action.payload
         },
-        removeEpisode: (state, action: {type: string, payload: {epId: number, userId: string, liked: FAV}}) => {
-            // const { epId, userId, liked } = action.payload
-            // console.log( state.likedEpisodes.indexOf(liked))
-            // const temp = state.likedEpisodes.filter((item) => userId !== item.user_id && epId !== item.episode_id)
-            // state.likedEpisodes = temp
-            // console.log(epId, userId)
-            // console.log( state.likedEpisodes.indexOf(liked))
-        }
+        removeEpisode: (state, action: {type: string, payload: { episode: EPISODE, showTitle: string }}) => {
+            const { episode, showTitle } = action.payload
+
+            state.likedEpisodes = state.likedEpisodes.filter((item) => showTitle === item.showTitle && episode.episode === item.episode.id)
+            state.isliked = false
+            state.likedEp = initialState.likedEp
+            console.log(action)
+        },
+        likeDislike: (state, action: { type: string, payload: boolean}) => {
+            state.isliked = action.payload
+        },
     }
 })
 
 export const {
     AddToLikedEpisodes,
     SetAllLikedEpisodes,
-    removeEpisode
+    removeEpisode,
+    likeDislike,
 } = favoriteSlice.actions
 
 export default favoriteSlice.reducer
